cac57c7391d2f0d9d1e11cb503ca2225de479ee6
Chenshiwen <cswzmg@live.com>
2017-05-12 15:28:28 +0800

    Initial Commit

---
+++ OpenGLES3.5多重纹理02/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OpenGLES3.5多重纹理02
+//
+//  Created by ShiWen on 2017/5/12.
+//  Copyright © 2017年 ShiWen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OpenGLES3.5多重纹理02/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenGLES3.5多重纹理02
+//
+//  Created by ShiWen on 2017/5/12.
+//  Copyright © 2017年 ShiWen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
